9da1473f9dfb79cb508b9fcbda9de0de9a2c6256
ytnobody <ytnobody@gmail.com>
2014-09-03 17:55:15 +0900

    version++

--- lib/WebService/Livedoor/Weather.pm
+++ lib/WebService/Livedoor/Weather.pm
@@ -8,7 +8,7 @@ use Encode;
 use URI::Fetch;
 use XML::Simple; 
 use JSON 2;
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use constant BASE_URI        => $ENV{LDWEATHER_BASE_URI} || 'http://weather.livedoor.com';
 use constant ENDPOINT_URI    => $ENV{LDWEATHER_ENDPOINT_URI} || BASE_URI. '/forecast/webservice/json/v1';
